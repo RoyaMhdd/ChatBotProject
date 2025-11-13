2806e7a4e21ecdccf34d1059262ab15562262f7e
RoyaMhdd <royamohammadi916@gmail.com>
2025-11-13 22:40:35 +0330

    Otp send fix

--- users/views.py
+++ users/views.py
@@ -5,15 +5,15 @@ from .models import User, OTP
 import secrets
 import requests
 
-KAVENEGAR_API_KEY = "YOUR_KAVENEGAR_API_KEY"  # جایگزین با کلید واقعی
+KAVENEGAR_API_KEY = "476C4D54596B6744474A4A314A72746634424774556D373174337430317A392F34685054774543783759493D"  # جایگزین با کلید واقعی
 
-@csrf_exempt
 def Home(request):
     return render(request, 'home.html')
 
 def verify(request):
     phone_number = request.GET.get('phone', '')
     return render(request, 'code.html', {'phone_number': phone_number})
+
 
 @csrf_exempt
 def send_otp(request):
@@ -24,30 +24,43 @@ def send_otp(request):
     if not phonenumber or len(phonenumber) != 11 or not phonenumber.startswith('09'):
         return JsonResponse({"error": "Invalid phone number"}, status=400)
 
+
     user, _ = User.objects.get_or_create(phonenumber=phonenumber)
     OTP.objects.filter(user=user).delete()
+
+
     code = str(secrets.randbelow(9000) + 1000)
     OTP.objects.create(user=user, code=code)
 
 
     formatted_number = '+98' + phonenumber[1:]
+
+
     try:
         response = requests.get(
             f"https://api.kavenegar.com/v1/{KAVENEGAR_API_KEY}/verify/lookup.json",
             params={
-                'receptor': formatted_number,
-                'token': code,
-                'template': 'verify-code'
+                "receptor": formatted_number,
+                "token": code,
+                "template": "verifyy"
             },
             timeout=10
         )
-        if response.status_code != 200:
-            print("Error", response.text)
-    except Exception as e:
-        print("No connection", e)
 
-    
+        result = response.json()
+
+        if result['return']['status'] != 200:
+            print("❌ خطا در ارسال OTP:", result)
+            return JsonResponse({"error": "ارسال پیامک با خطا مواجه شد."}, status=500)
+        else:
+            print("✅ OTP sent successfully:", code)
+
+    except requests.RequestException as e:
+        print(" خطای ارتباط با کاوه نگار:", e)
+        return JsonResponse({"error": "عدم ارتباط با سرور پیامک."}, status=500)
+
     return redirect(f'/verify/?phone={phonenumber}')
+
 
 @csrf_exempt
 def verify_otp(request):
